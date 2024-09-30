466023ec3eaefb28d2e50c8cf5bfff195c74a692
Flitshcl1 <Timfischer1212@gmail.com>
2024-09-30 17:50:53 +0200

    First healthBarSystem

--- Assets/Scripts/HealthSlider.cs
+++ Assets/Scripts/HealthSlider.cs
@@ -7,16 +7,24 @@ public class HealthSlider : MonoBehaviour
 {
     public Slider slider;  // Referenz auf den Slider
 
+    public Gradient gradient; //Farbe
+
+    public Image fill;
+
     // Setze die maximale Gesundheit im Slider
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     // Aktualisiere die aktuelle Gesundheit im Slider
     public void Sethealth(int health)
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
