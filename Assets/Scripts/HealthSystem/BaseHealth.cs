3534e95ae5e385853eff7ffdabc7c411453158e3
Flitshcl1 <Timfischer1212@gmail.com>
2024-10-05 12:41:16 +0200

    trfouz

--- Assets/Scripts/HealthSystem/BaseHealth.cs
+++ Assets/Scripts/HealthSystem/BaseHealth.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class BaseHealth : MonoBehaviour
 {
+    public float health;
+    public Transform enemy1;
+    public Transform enemy2;
+    public Transform enemy3;
+
+    public int damage;
+    public float detectionRange = 1f; //Wie weit entfernt erhält die Base schaden? --siehe public
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,25 @@ public class BaseHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckEnemyProximity(enemy1);
+        CheckEnemyProximity(enemy2);
+        CheckEnemyProximity(enemy3);
+    }
+
+    void CheckEnemyProximity(Transform enemy)
+    {
+        if(Vector3.Distance(transform.position, enemy.position) < detectionRange)
+        {
+            TakeDamage(damage);
+        }
+    }
+
+    void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
