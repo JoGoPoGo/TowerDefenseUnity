e4a47ec3fc5ddf0ad31f297c69be37008661e165
Flitshcl1 <Timfischer1212@gmail.com>
2025-02-16 13:51:45 +0100

    dcfgksdgsgjfhgxgchxh

--- Assets/Scripts/UI/SwitchText.cs
+++ Assets/Scripts/UI/SwitchText.cs
@@ -3,34 +3,52 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SwitchText : MonoBehaviour
 {
-    public Text[] textA;
+    public TMP_Text[] textA;
     public int textnumber;
     public Button next;
+    public int skip = 1;
 
-    private Text currentText;
+    private TMP_Text startText;
+    private int currentIndex;
 
     void Start()
     {
-        currentText = textA[textnumber];
+        startText = textA[textnumber];
         if (next != null)
         {
             next.onClick.AddListener(OnNextClick);
         }
+        foreach (TMP_Text txt in textA)
+        {
+            txt.gameObject.SetActive(false);
+        }
+        startText.gameObject.SetActive(true);
     }
     void OnNextClick() // Blättert zur nächsten Seite
     {
-        Text nextText = textA[textnumber + 1]; 
-        currentText.gameObject.SetActive(false);
-        nextText.gameObject.SetActive(true);
-        textnumber++;
+        int nextIndex = textnumber + skip; // berechnet den index des nächsten Textes
+        Debug.Log(nextIndex);
+        if (nextIndex >= 0 && nextIndex <= textA.Length)
+        {
+            TMP_Text nextText = textA[textnumber + skip];
+            startText.gameObject.SetActive(false);
+            if (startText.isActiveAndEnabled)
+            {
+                Debug.Log("currentText ist aktiv");
+            }
+            nextText.gameObject.SetActive(true);
+            textnumber = nextIndex;
+        }
+        Debug.Log(nextIndex);
     }
     public void loadtext (int number)
     {
-        Text loadingText = textA[number];
-        foreach (Text txt in textA)
+        TMP_Text loadingText = textA[number];
+        foreach (TMP_Text txt in textA)
         {
             txt.gameObject.SetActive(false );
         }
