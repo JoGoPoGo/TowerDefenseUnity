bc4deaecd1206bad90e9c0520f10aa8e428355ac
Flitshcl1 <Timfischer1212@gmail.com>
2025-05-18 09:14:02 +0200

    hA

--- Assets/Scripts/Towers/Specific Tower/Hochmut.cs
+++ Assets/Scripts/Towers/Specific Tower/Hochmut.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hochmut : MonoBehaviour
+public class Hochmut : Tower
 {
     // Start is called before the first frame update
     void Start()
