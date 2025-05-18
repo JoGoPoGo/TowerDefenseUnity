99d30a54d61ca08a9fa5206d4804a7da617456f5
Flitshcl1 <Timfischer1212@gmail.com>
2025-05-18 12:44:48 +0200

    18.5: Hochmut Minimum Range

--- Assets/Scripts/Towers/Specific Tower/Hochmut.cs
+++ Assets/Scripts/Towers/Specific Tower/Hochmut.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Hochmut : Tower
 {
-    // Start is called before the first frame update
-    void Start()
+    public float rangeMinimum;
+    
+    protected override void UpdateTarget()
     {
-        
-    }
+        // Sucht nach allen Gegnern mit dem Tag "Enemy"
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Finde den nächsten Gegner innerhalb der Reichweite
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance && distanceToEnemy > rangeMinimum)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        // Wenn ein Gegner gefunden wurde, setze ihn als Ziel
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy;
+        }
+        else
+        {
+            target = null; // Kein Gegner in Reichweite
+        }
     }
 }
