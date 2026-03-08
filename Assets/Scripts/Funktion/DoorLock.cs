712fc210d16e92110816c1bf9e82190fa56eeafc
Flitshcl1 <Timfischer1212@gmail.com>
2026-03-08 17:24:15 +0100

    DoorOpen

---
+++ Assets/Scripts/Funktion/DoorLock.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour
+{
+    public int currentMaxLevel;
+
+    private int stars;
+    DoorOpen[] allDisplays;
+
+    void Start()
+    {
+        stars = ProgressManager.Instance.GetCollectedStars();
+        Debug.Log(stars);
+
+        LevelsToLock();
+    }
+
+    public void LevelsToLock()
+    {
+
+        int smallestLockedLevel = 2;
+
+        if (stars >= 2)
+            smallestLockedLevel = 3;
+        if (stars >= 5)
+            smallestLockedLevel = 4;
+        if (stars >= 8)
+            smallestLockedLevel = 5;
+        if (stars >= 10)
+            smallestLockedLevel = 6;
+        if (stars >= 13)
+            smallestLockedLevel = 7;
+        if (stars >= 16)
+            smallestLockedLevel = 8;
+        if (stars >= 20)
+            smallestLockedLevel = 9;
+        if (stars >= 20)
+            smallestLockedLevel = 10;
+        if (stars >= 22)
+            smallestLockedLevel = 11;
+        if (stars >= 23)
+            smallestLockedLevel = 12;
+        if (stars >= 24)
+            smallestLockedLevel = 13;
+        if (stars >= 25)
+            smallestLockedLevel = 14;
+        if (stars >= 26)
+            smallestLockedLevel = 15;
+
+        if (currentMaxLevel < smallestLockedLevel)
+        {
+            currentMaxLevel = smallestLockedLevel;
+            Debug.LogWarning("Zu niedriger currentMaxlevel at LevelLock");
+        }
+
+
+        for (int i = smallestLockedLevel; i <= currentMaxLevel; i++)
+        {
+            lockLevel(i);
+        }
+    }
+    void lockLevel(int level)
+    {
+        allDisplays = FindObjectsOfType<DoorOpen>();
+        ProgressManager.Instance.LockLevel(level);
+        foreach (DoorOpen s in allDisplays)
+        {
+            if (s.lvlNumber == level)
+            {
+                Debug.Log("LevelLocked" + level + " at 63");
+                s.isLocked = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Funktion/DoorOpen.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public float moveRight = 2f;     // Wie weit die Tür nach rechts fährt
+    public float speed = 2f;         // Bewegungsgeschwindigkeit
+    public bool isLocked = false;
+    public int lvlNumber = 0;
+
+    private Vector3 closedPosition;
+    private Vector3 openPosition;
+    private bool isOpen = false;
+    private bool isMoving = false;
+
+    void Start()
+    {
+        closedPosition = transform.position;
+        openPosition = closedPosition + transform.right * moveRight;
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && !isMoving && !isLocked) // Rechtsklick
+        {
+            if (isOpen)
+                StartCoroutine(MoveDoor(closedPosition));
+            else
+                StartCoroutine(MoveDoor(openPosition));
+
+            isOpen = !isOpen;
+        }
+    }
+
+    System.Collections.IEnumerator MoveDoor(Vector3 target)
+    {
+        isMoving = true;
+
+        while (Vector3.Distance(transform.position, target) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                target,
+                speed * Time.deltaTime
+            );
+
+            yield return null;
+        }
+
+        transform.position = target;
+        isMoving = false;
+    }
+}
