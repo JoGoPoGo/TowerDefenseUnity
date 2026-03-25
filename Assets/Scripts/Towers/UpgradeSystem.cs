34fbc070a06aac0c92d1db73228632bf3e569794
Flitshcl1 <Timfischer1212@gmail.com>
2026-03-25 22:17:35 +0100

    d

---
+++ Assets/Scripts/Towers/UpgradeSystem.cs
@@ -0,0 +1,17 @@
+using DG.Tweening.Core.Easing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Cinemachine.DocumentationSortingAttribute;
+
+public class UpgradeSystem : MonoBehaviour
+{
+    [Header("Referenzes")]
+    public Tower thisTower;
+
+    [Header("Upgrade")]
+    int damageUp;
+
+    // Start is called before the first frame update
+ 
+}
